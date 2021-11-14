eb28bc2265b7efd484e8e538f02349dc794ed1b7
ikerobando19 <92729041+ikerobando19@users.noreply.github.com>
2021-11-14 15:26:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
+int main(){
+    int n,x=0,y=1,z=1;
+    cout<< "inserisci il numero di elementi";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        z = x + y;
+        cout<<z<<" ";
+        x = y;
+        y = z;
 
-int main() {
-   return 0;
+    }
+    cout<<"\n";
+    system("pause");
+
+    return 0;
 }
